82da83c38652fc1544d69c4d8577fecd345dd9ce
杨玉峰 <1539558157@qq.com>
2020-01-17 18:07:08 +0800

    修改

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,36 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="title">短链接平台</div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
   name: 'app',
   components: {
-    HelloWorld
   }
 }
 </script>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style lang="stylus">
+*
+  padding 0
+  margin 0
+html,body
+  width 100%
+  height 100%
+#app
+  width 100%
+  height 100%
+  background #fcded4
+  .title
+    width 100%
+    height 80px
+    text-align center
+    font-size 55px
+    position relative
+    top 10%
+    letter-spacing 5px
+    color #fcd8d4
+    text-shadow 0 0 0 #b23131, 0 0 3px #b23131, 0 0 6px #b23131, 0 0 9px #b23131, 0 0 12px #b23131
 </style>
